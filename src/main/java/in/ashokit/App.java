a8c0b36e33094bedde3c44c228f143341b09df25
gurunadha <degalaguru7@gmail.com>
2023-07-22 21:09:13 +0530

    modifide

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -1,13 +1,13 @@
 package in.ashokit;
 
 /**
- * Hello world!
+ * Hello world is so beautiful!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World is so beautiful!" );
     }
 }
